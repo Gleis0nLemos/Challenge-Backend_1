47cd209355400c62d1e3e75b129e7b0e5ed0ca36
Gleison <gleison04lemos@gmail.com>
2022-12-06 15:41:48 -0300

    feat: POST, GET request via API

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from vflix.views import VideoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('videos', VideoViewSet, basename='Videos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
--- vflix/views.py
+++ vflix/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from vflix.models import Video
+from vflix.serializer import VideoSerializer
 
-# Create your views here.
+class VideoViewSet(viewsets.ModelViewSet):
+    """Viewsets for Video"""
+
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
